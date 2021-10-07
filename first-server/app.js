629b56916459a9c6366ab0d1e824682e68b2f9a7
MalvinWeiss <malvin86100@gmail.com>
2021-10-07 14:19:53 +0200

    changement

--- first-server/app.js
+++ first-server/app.js
@@ -8,6 +8,11 @@ router.get('/',function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
+router.get('/page1',function(req,res){
+    res.sendFile(path.join(__dirname+'/page1.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 8080);
